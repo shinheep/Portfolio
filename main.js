8fc26db4462b84b45b0e6f5ef1428a414b09e65b
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-10-11 14:40:22 -0700

    hamburger icon and nav bar somewhat settled

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 // $('h3').animate({left: '250px'});
 
-/* typewriter effect */
-const i=0;
-const text = "hello";
-const speed = 50;
-
-function typeWriter() {
-    if (i < text.length) {
-        $('.skillsUnderHeader').innerHTML += text.charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-}
+$('.hamburger').on('click', function(event){
+    event.preventDefault();
+    $('.navbar').toggle("display");
+  });
