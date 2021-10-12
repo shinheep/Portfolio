07c4fcdd3a1c1510415cb1638348be6f5de0f2a4
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-10-12 11:55:40 -0700

    saving changes before lunch

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // $('h3').animate({left: '250px'});
 
+
+// Toggling Hamburger Menu to Display/Hide NavBar
 $('.hamburger').on('click', function(event){
+    
+    console.log('click worked!')
     event.preventDefault();
     $('.navbar').toggle("display");
   });
-
-  
