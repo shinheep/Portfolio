d88ded5ab8b650727089fe3aa82f5e9e81555ab3
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-10-12 22:30:03 -0700

    some css and js changes

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // $('h3').animate({left: '250px'});
 
+$('.bouncy').on('click', function(event){
+    event.preventDefault();
+    $('.bouncy').toggle('bounce', {times:10}, 'slow');
+})
+
 
 // Toggling Hamburger Menu to Display/Hide NavBar
 $('.hamburger').on('click', function(event){
@@ -7,7 +12,44 @@ $('.hamburger').on('click', function(event){
     $('.navbar').toggle("display");
   });
 
-$('.bouncy').on('click', function(event){
-    event.preventDefault();
-    $('.bouncy').toggle('bounce', {times:10}, 'slow');
-})
+
+//Changing text color upon hover//
+$('#engineer').mouseover(function(event){
+    $('#engineer').css("color", "blue")
+});
+
+$('#engineer').mouseleave(function(event){
+    $('#engineer').css("color", "white")
+});
+
+$('#developer').mouseover(function(event){
+    $('#developer').css("color", "blue")
+});
+
+$('#developer').mouseleave(function(event){
+    $('#developer').css("color", "white")
+});
+
+$('#programmer').mouseover(function(event){
+    $('#programmer').css("color", "blue")
+});
+
+$('#programmer').mouseleave(function(event){
+    $('#programmer').css("color", "white")
+});
+
+$('#coder').mouseover(function(event){
+    $('#coder').css("color", "blue")
+});
+
+$('#coder').mouseleave(function(event){
+    $('#coder').css("color", "white")
+});
+
+$('#dog-lover').mouseover(function(event){
+    $('#dog-lover').css("color", "magenta")
+});
+
+$('#dog-lover').mouseleave(function(event){
+    $('#dog-lover').css("color", "white")
+});
