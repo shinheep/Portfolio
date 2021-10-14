07992b0f7002d49c2ce5928e2e66231e3ca39da8
Shinhee Park <shinheepark@Shinhees-MacBook-Air.local>
2021-10-14 16:37:02 -0700

    noticed an error in last commit

--- main.js
+++ main.js
@@ -11,3 +11,16 @@ $('.hamburger').on('click', function(event){
     event.preventDefault();
     $('.navbar').toggle("display");
   });
+
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
